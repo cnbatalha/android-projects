a5b2914961c52bf5b2efbcff9b0675c3136d04f6
Carlos Nascimento <cnbatalha@gmail.com>
2016-10-10 16:43:30 -0400

    alterando projeto, retirando erros

--- leitor-noticias/LeitorRSS/app/src/main/java/leitorrss/cnbatalha/leitorrss/adapter/AdapterListView.java
+++ leitor-noticias/LeitorRSS/app/src/main/java/leitorrss/cnbatalha/leitorrss/adapter/AdapterListView.java
@@ -7,17 +7,18 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import com.leitorrss.cnbatalha.R;
-import com.leitorrss.model.cnbatalha.item;
 
 import java.util.List;
 
+import leitorrss.cnbatalha.leitorrss.R;
+import leitorrss.cnbatalha.leitorrss.model.Item;
+
 public class AdapterListView extends BaseAdapter {
 
-	private List<item> Items;
+	private List<Item> Items;
 	private LayoutInflater mInflater;
 
-	public AdapterListView(Context context, List<item> items) {
+	public AdapterListView(Context context, List<Item> items) {
 		super();
 		this.Items = items;
 
@@ -50,21 +51,22 @@ public class AdapterListView extends BaseAdapter {
 		// se a view estiver nula (nunca criada), inflamos o layout nela.
 		if (convertView == null) {
 			// infla o layout para podermos pegar as views
-			convertView = mInflater.inflate(R.layout.item_listview, null);
+			//convertView = mInflater.inflate(R.layout.item_listview, null);
 			// cria um item de suporte para n�o precisarmos sempre
 			// inflar as mesmas informacoes
 			// itemHolder = new ItemSuporte();
 			// itemHolder.txtTitle = ((TextView) view.findViewById(R.id.text));
 			// itemHolder.imgIcon = ((ImageView)
 			// view.findViewById(R.id.imagemview));
-			txtView = (TextView) convertView.findViewById(R.id.txtItem);
+            // TODO: review layout
+			txtView = (TextView) convertView.findViewById(R.id.action_settings);
 			// define os itens na view;
 			convertView.setTag(txtView);
 		} else { // se a view j� existe pega os itens.
 			txtView = (TextView) convertView.getTag();
 		}
 
-		item itm = (item) Items.get(position);
+		Item itm = (Item) Items.get(position);
 		txtView.setText(itm.title);
 		
 		return convertView;
