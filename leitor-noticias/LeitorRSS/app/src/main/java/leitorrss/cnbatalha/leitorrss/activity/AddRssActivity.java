ff74f084db10cac4f9e30f805065f6ee56f45720
Carlos Nascimento <cnbatalha@gmail.com>
2016-11-09 22:26:43 -0400

    add acty to add rss

---
+++ leitor-noticias/LeitorRSS/app/src/main/java/leitorrss/cnbatalha/leitorrss/activity/AddRssActivity.java
@@ -0,0 +1,16 @@
+package leitorrss.cnbatalha.leitorrss.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import leitorrss.cnbatalha.leitorrss.R;
+
+public class AddRssActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rss);
+    }
+
+}
